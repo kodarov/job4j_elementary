cb7a7aa839d1d57c8b0531baa2b45cc7b6e4dfef
kodarov_salavat <kodarov@gmail.com>
2023-12-26 00:38:07 +0400

    3.1. Переназначение переменной [#232354]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -18,5 +18,11 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        //3.1. Переназначение переменной [#232354]
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println("size = " + size);
     }
 }
