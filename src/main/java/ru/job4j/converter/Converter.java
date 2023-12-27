758de9aa2703c201d06e2aa225fe3639de93b322
kodarov_salavat <kodarov@gmail.com>
2023-12-27 20:27:48 +0400

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -22,14 +22,27 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(120);
-        float euroToRuble = Converter.euroToRuble(2);
-        float dollarToRuble = Converter.dollarToRuble(2);
+        float input1 = 140;
+        float input2 = 120;
+        float input3 = 2;
 
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("120 rubles are " + dollar + " dollar.");
-        System.out.println("2 euro are " + euroToRuble + " rubles.");
-        System.out.println("2 dollar are " + dollarToRuble + " rubles.");
+        float euro = Converter.rubleToEuro(input1);
+        float dollar = Converter.rubleToDollar(input2);
+        float euroToRuble = Converter.euroToRuble(input3);
+        float dollarToRuble = Converter.dollarToRuble(input3);
+
+        float expected1 = 2;
+        float expected2 = 140;
+        float expected3 = 120;
+
+        boolean passed1 = expected1 == euro;
+        boolean passed2 = expected1 == dollar;
+        boolean passed3 = expected2 == euroToRuble;
+        boolean passed4 = expected3 == dollarToRuble;
+
+        System.out.println("140 rubles are " + euro + " euro. Test result " + passed1);
+        System.out.println("120 rubles are " + dollar + " dollar. Test result " + passed2);
+        System.out.println("2 euro are " + euroToRuble + " rubles. Test result " + passed3);
+        System.out.println("2 dollar are " + dollarToRuble + " rubles. Test result " + passed4);
     }
 }
