6c6ae1fb67e6b074d29b191665b284ad7692298c
kodarov_salavat <kodarov@gmail.com>
2023-12-29 00:25:15 +0400

    0. Что такое тестирование [#159431 #469078]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -22,27 +22,24 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float input1 = 140;
-        float input2 = 120;
-        float input3 = 2;
+        float input = 140;
+        float expected = 2;
+        float euro = Converter.rubleToEuro(input);
+        System.out.println(input + " rubles are " + euro + " euro. Test result " + (expected == euro));
 
-        float euro = Converter.rubleToEuro(input1);
-        float dollar = Converter.rubleToDollar(input2);
-        float euroToRuble = Converter.euroToRuble(input3);
-        float dollarToRuble = Converter.dollarToRuble(input3);
+        input = 4;
+        expected = 280;
+        float euroToRuble = Converter.euroToRuble(input);
+        System.out.println(input + " euro are " + euroToRuble + " rubles. Test result " + (expected == euroToRuble));
 
-        float expected1 = 2;
-        float expected2 = 140;
-        float expected3 = 120;
+        input = 180;
+        expected = 3;
+        float dollar = Converter.rubleToDollar(input);
+        System.out.println(input + " rubles are " + dollar + " dollar. Test result " + (expected == dollar));
 
-        boolean passed1 = expected1 == euro;
-        boolean passed2 = expected1 == dollar;
-        boolean passed3 = expected2 == euroToRuble;
-        boolean passed4 = expected3 == dollarToRuble;
-
-        System.out.println("140 rubles are " + euro + " euro. Test result " + passed1);
-        System.out.println("120 rubles are " + dollar + " dollar. Test result " + passed2);
-        System.out.println("2 euro are " + euroToRuble + " rubles. Test result " + passed3);
-        System.out.println("2 dollar are " + dollarToRuble + " rubles. Test result " + passed4);
+        input = 5;
+        expected = 300;
+        float dollarToRuble = Converter.dollarToRuble(input);
+        System.out.println(input + " dollar are " + dollarToRuble + " rubles. Test result " + (expected == dollarToRuble));
     }
 }
