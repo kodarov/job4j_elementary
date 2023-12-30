274abf23f4be4ee1ea5eab64ff3e1c3c2a21689e
kodarov_salavat <kodarov@gmail.com>
2023-12-30 05:26:11 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,5 +1,8 @@
 package ru.job4j.array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -9,5 +12,16 @@ public class ArrayDefinition {
         System.out.println("ages length: " + ages.length);
         System.out.println("surnames length: " + surnames.length);
         System.out.println("prices length: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Nick";
+        names[2] = "Dennis";
+        names[3] = "Alex";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
