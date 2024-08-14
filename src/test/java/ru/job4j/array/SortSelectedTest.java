ccb176454ad650a794781e2d2b9d1193a8ac4d8a
kodarov_salavat <kodarov@gmail.com>
2024-08-14 19:21:04 +0400

    GPT. Нейросети спешат на помощь. [#505156]

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -28,4 +28,28 @@ public class SortSelectedTest {
         int[] expected = new int[]{1, 2, 3};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    public void whenSortSingleElementArray() {
+        int[] data = new int[]{1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortEmptyArray() {
+        int[] data = new int[]{};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortReverseSortedArray() {
+        int[] data = new int[]{5, 4, 3, 2, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[]{1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
 }
